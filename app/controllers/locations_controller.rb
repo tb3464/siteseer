8f61a5948634c2eb64d9ae22e815cc2e57218dd8
Anthony Bell <tb12485@yahoo.com>
2011-07-14 03:47:58 -0500

    first commit

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,65 @@
+class LocationsController < ApplicationController
+
+  def index
+    if params[:search].present?
+      @locations = Location.near(params[:search], 50, :order => :distance)
+    else
+      @locations = Location.all
+    end
+  end
+
+  def show
+    @location = Location.find(params[:id])
+  end
+
+  def new
+    @location = Location.new
+  end
+
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def create
+    @location = Location.new(params[:location])
+
+    respond_to do |format|
+      if @location.save
+        format.html { redirect_to(@location, :notice => 'Location was successfully created.') }
+        format.xml  { render :xml => @location, :status => :created, :location => @location }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /locations/1
+  # PUT /locations/1.xml
+  def update
+    @location = Location.find(params[:id])
+
+    respond_to do |format|
+      if @location.update_attributes(params[:location])
+        format.html { redirect_to(@location, :notice => 'Location was successfully updated.') }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @location.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /locations/1
+  # DELETE /locations/1.xml
+  def destroy
+    @location = Location.find(params[:id])
+    @location.destroy
+
+    respond_to do |format|
+      format.html { redirect_to(locations_url) }
+      format.xml  { head :ok }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Siteseer::Application.config.secret_token = 'ff980ba1de3bb780c893c1046be1fb64017803bfc7c87777822b57ea3ff5dda86251a01594f582760f2401bed3a843afea507bc850d8be714dd1ae05999eb213'
